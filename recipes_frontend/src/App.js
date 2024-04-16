b104ef6efd957b5cfc9520cc0353a6e18a2deff9
Rakshit <55257622+rakshit-goel-cse@users.noreply.github.com>
2024-04-16 22:21:48 +0530

    frontend basic changes

---
+++ recipes_frontend/src/App.js
@@ -0,0 +1,66 @@
+
+import Search from './pages/Search.js';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [options, setoptions] = useState([]);
+  const [serverStatus, setServerStatus] = useState("Wait");
+
+  useEffect(() => {
+    const fetchData=()=>{
+      let url='http://192.168.29.231:1234/options';
+  
+      fetch(url,{
+        headers: {
+          'Accept': 'application/json'
+        }
+      })
+      .then(response=>{
+        // Check if response is okay
+      if (!response.ok) {
+        setServerStatus("error");
+        throw new Error('Network response was not ok');
+      }
+      // Parse response body as JSON and return the parsed data
+      else{
+        setServerStatus("OK");
+        return response.json();
+      }
+    })
+      .then(data=>{
+        setoptions(data);
+      })
+      .catch(error=>{
+        setServerStatus("error");
+        console.error(error);
+      })
+    }
+
+    
+      fetchData();
+    
+  }, [])
+  
+  const callPages=()=>{
+    if(serverStatus==="Wait"){
+      return <view style={{color:"green"}}>Wait</view>;
+    }
+    else{
+      if(serverStatus==="error"){
+        return <view style={{color:"Red"}}>Error</view>;
+      }
+      else{
+        return <Search Option={options} setServerStatus={setServerStatus}/>;
+      }
+    }
+  }
+
+  return (
+    <>
+      {callPages()}
+    </>
+  );
+}
+
+export default App;
---
+++ recipes_frontend/src/pages/Search.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import './search.css';
+
+function Search({Option, setServerStatus}) {
+    const [searchText, setsearchText] = useState("");
+    const [course,setCourse] = useState('');
+    const [diet,setDiet] = useState('');
+    const [cuisines,setCuisines] = useState('');
+    const [searchData, setSearchData] = useState('');
+
+    const [page, setpage] = useState(1);
+    const [totalPage, settotalPage] = useState(0);
+    const [offSet,setOffSet] = useState(10);
+
+    const getDataFromServer=()=>{
+        console.log("serching data");
+        let url="http://192.168.29.231:1234/recipes";
+        
+        setSearchData("Searching");
+
+        fetch(url,{
+            headers: {
+                'Accept': 'application/json'
+              }
+            ,parameters:{
+                'Course':course,
+                'Diet':diet,
+                'Cuisine':cuisines,
+                'SearchText':searchText
+            }
+        })
+        .then(response=>{
+            if(!response.ok){
+                setSearchData("Error searching");
+            }
+            else{
+                return response.json();
+            }
+        })
+        .then(data=>{
+            //console.log("search data- "+JSON.stringify(data));
+            //let dataList=JSON.parse(data);
+            setSearchData(data);
+            settotalPage(data.length/offSet);
+        })
+        .catch(error=>{
+            setSearchData('');
+            console.error(error);
+            setServerStatus("error");
+        })
+    }
+
+    const optionElement=(options,state,setState,id,heading)=>{
+        return(
+            <select key={id} value={state} onChange={(event)=>setState(event.target.value)}>
+            <option value="">{heading}</option>
+            {options.map((value,index)=>{
+                return(<option key={index} value={value}>{value}</option>)
+            })}
+          </select>
+        );
+    }
+
+    return(
+        <div className='container'>
+            {Option && Option.diet && optionElement(Option.diet, diet , setDiet ,1,"Diet")}
+            {Option && Option.course && optionElement(Option.course, course , setCourse ,2,"Course")}
+            {Option && Option.cuisines && optionElement(Option.cuisines, cuisines , setCuisines ,3,"Cuisines")}
+            
+            <input type='submit' onClick={getDataFromServer}/>
+            
+            <input className='search' type="text" onChange={(event)=>setsearchText(event.target.value)} value={searchText} placeholder="Search keywords"></input>
+            
+            <view style={{height:'20%',maxHeight:'20%' , overflow:'auto'}}
+            >{JSON
+            .stringify(searchData
+                .slice(offSet*(page-1),offSet*page)
+                )}</view>
+            
+            <view>
+                    <label>offSet</label>
+                    <input type={'number'} value={offSet} onChange={(event)=>{
+                     const newOffset = parseInt(event.target.value);
+                     setOffSet(newOffset);
+                     settotalPage(Math.ceil(searchData.length / newOffset));
+                     setpage(1);
+                    }}></input>
+                <input type='button' onClick={()=>page>1?setpage(page-1):''}value={"<<"}/>
+                <input type={'text'} value={page} onChange={(event)=>{
+                    setpage(event.target.value?event.target.value:0)
+                    }}></input>
+                <input type='button' onClick={()=>page<totalPage ?setpage(page+1):''} value={">>"}/>
+            </view>
+        </div>
+    );
+}
+
+export default Search;
